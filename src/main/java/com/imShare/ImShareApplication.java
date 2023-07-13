67a0f1064179fa068d95a978725af4c4f5098523
duylue <binhminhbenem2801@gmail.com>
2023-07-13 20:58:29 +0700

    first commit

---
+++ src/main/java/com/imShare/ImShareApplication.java
@@ -0,0 +1,13 @@
+package com.imShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImShareApplication.class, args);
+	}
+
+}
