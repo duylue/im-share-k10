1f446aec9ee1dd9b5e7904051aac30bbd344b7b7
Admin <vinhrong.vr.vr@gmail.com>
2023-07-20 20:14:46 +0700

    vinh model

--- src/main/java/com/imShare/model/UserLike.java
+++ src/main/java/com/imShare/model/UserLike.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Like1 {
+public class UserLike {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int likeId;
