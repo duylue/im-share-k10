bd48206b32b2ff932a8b5406f4416b8763557874
phantu202“git config --global user.email phantu202@gmail.com“ <125955700+phantu202@users.noreply.github.com>
2023-07-26 14:51:55 +0700

    Tu-create post

--- src/main/java/com/imShare/controller/api/StorageController.java
+++ src/main/java/com/imShare/controller/api/StorageController.java
@@ -4,23 +4,21 @@ import com.imShare.service.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/storages")
+@CrossOrigin("*")
+@RequestMapping("/api/storages")
 public class StorageController {
 
     @Autowired
     private StorageService service;
 
     @PostMapping("/attachments")
-    public ResponseEntity<?> createAttachments(@RequestPart(value = "attachments") List<MultipartFile> files) {
+    public ResponseEntity<?> createAttachments(@RequestParam("attachments") List<MultipartFile> files) {
         return service.createAttachment(files);
     }
 }
